92bbb6dedabfb08aa891740fab222a48c7f71a16
edwborges <eduardo.borges19646201@gmail.com>
2023-11-14 16:12:53 -0300

    Refatorando: removendo linhas de códigos redundantes nas entidades

--- src/main/java/adocaopets/models/Tutor.java
+++ src/main/java/adocaopets/models/Tutor.java
@@ -20,24 +20,19 @@ public class Tutor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @NotBlank
-    @Column(name = "nome")
     private String nome;
 
     @NotBlank
     @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
-    @Column(name = "telefone")
     private String telefone;
 
     @NotBlank
     @Email
-    @Column(name = "email")
     private String email;
 
     @OneToMany(mappedBy = "tutor", fetch = FetchType.EAGER)
-    @JsonManagedReference("tutor_adocoes")
     private List<Adocao> adocoes;
 }

6ad65d5309ec9f1017c52c35ce9b623b81f89d28
edwborges <eduardo.borges19646201@gmail.com>
2023-11-14 18:00:53 -0300

    Implementando o OpenAPI no projeto

---
+++ src/main/java/adocaopets/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package adocaopets.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(contact = @Contact(
+                name = "Eduardo Borges",
+                email = "contact.edwborges@gmail.com",
+                url = "https://github.com/oEduardoBorges"
+        ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+        }
+)
+public class OpenApiConfig {
+}
